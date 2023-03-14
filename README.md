88142ca1b55235c61f853e3512eb00c3d744c483
Jakob-Dove <hackerzfuel@gmail.com>
2023-03-13 19:31:45 -0500

    More for loops!

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300806&assignment_repo_type=AssignmentRepo)
-### First Commit
+### second Commit
 
-I hope to have squares that fit on the screen for the next for loop.  colors haven't been picked but there is a color palette that is similar to the sun rise or sun set.  Just a little behind as well, so I don't have a gif ready yet.
+Found a for loop that works. Unfortunately, I had to scrifice how big the squares were to make them fit on the page.
 
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -4,34 +4,52 @@ var a=100;
 var b=100;
 
 function draw_one_frame() {
-  background(93, 93, 93);
+	strokeWeight(1);
+  translate(width/2, height/2);
+
+  background(73, 73, 73);
   x+=2;
   y+=2;
 	a-=2;
 	b-=2;
 
-	var xpos = 275;
-	var ypos = -260;
+	let xpos = 35; //third set of squares from the left
+	let xpos2 = 270; //far right set of squares
+	let xpos3 = -210;//second set of squares on the left
+	let xpos4 = -460;//far left set of squares
+	
+	let ypos = -200;
 
 
 	color(226,220,208);
-	rect(745, 0, 120, height);  //Makes the 4 big rectangles in the background
+	rect(-480, -270, 120, height);  //The far left rectangle
 
-	rect(530, 0, 120, height);
+	rect(-240, -270, 120, height);	//The second from the left rectangle
 
-	rect(315, 0, 120, height);
+	rect(240, -270, 120, height);	//The third from the lef rectangle
 
-	rect(100, 0, 120, height);
+	rect(0, -270, 120, height);		//The far right rectangle
 
-// 	strokeWeight(1);
-//   translate(width/2, height/2);
-  for(var i=1;i> -5;i--){  //This creates the squares all over the page
-	  for(var j=1;j>-5;j--){ // This gives the y values for the squares
-		stroke(1);
-		rect(xpos*i, ypos*j, 100, 100);
-	  }
+
+  for(let j=1;j > -11;j--){ //third set of squares from the left
+	stroke(1);
+	rect(xpos, ypos*j, 50, 50);
   }
 
+  for(let j=1;j > -11;j--){ //far right set of squares
+	stroke(1);
+	rect(xpos2, ypos*j, 50, 50);
+  }
+
+  for(let j=1;j > -11;j--){ //second set of squares on the left
+	stroke(1);
+	rect(xpos3, ypos*j, 50, 50);
+  }
+
+  for(let j=1;j > -11;j--){ 	//far left set of squares
+	stroke(1);
+	rect(xpos4, ypos*j, 50, 50);
+  }
 }
 
 
