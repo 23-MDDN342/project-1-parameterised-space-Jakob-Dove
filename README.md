b661396d980fe3d6cd7215522a8d518c4ee6940a
Jakob-Dove <hackerzfuel@gmail.com>
2023-03-20 16:15:13 -0500

    Changed Color's and added squares in the for loop

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300806&assignment_repo_type=AssignmentRepo)
-### Third Commit
+### fourth Commit
 
-Got a map working.  changed the colors! Need to make the map less jittery! 
-
+changed some colors made the background colors more complimentory towards the moving squares.  I plan for the squares to move slower.  Trying to animate the colors of the background rectangles that the squares move on. THe colors still need work and the blue needs to be changed.  I feel like something is missing as well.  
 
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -10,15 +10,18 @@ function draw_one_frame(cur_frac) {
  
   fill(255,0,0);
 
-	// let from = color()
-	// let to = color()
+	let from = color(255,201,34);
+	let to = color(253,94, 83);
+
+	let interA = lerpColor(from, to, 0.3);
+	let interB = lerpColor(from, to, 0.6);
   
   //background(73, 73, 73);
   x+=2;
   y+=2;
 	a-=2;
 	b-=2;
-	let myMap = map(cur_frac, 0, 1, 0, width)
+	let myMap = map(cur_frac, 0, 2, 0, width)
 	let xpos = 30; //third set of squares from the left
 	let xpos2 = 275; //far right set of squares
 	let xpos3 = 515; //second set of squares on the left
@@ -27,7 +30,8 @@ function draw_one_frame(cur_frac) {
 	let ypos = -150;
 
 
-	color(226,220,208);
+	fill(interA);
+	// fill(interB);
 
 	
 	rect(0, 0, 120, height);  //The far left rectangle
@@ -35,30 +39,30 @@ function draw_one_frame(cur_frac) {
 	rect(240, 0, 120, height);	//The second from the left rectangle
 
 	rect(480, 0, 120, height);	//The third from the lef rectangle
-
+	fill(interB);
 	rect(720, 0, 120, height);		//The far right rectangle
 
 	
 	fill(255,121,79) //Orange squares
-  for(let j=1;j < 6;j++){ //third set of squares from the left
+  for(let j=1;j < 15;j++){ //third set of squares from the left
 	stroke(1);
 	rect(xpos, myMap*j, 50, 50);
   }
 
   fill(43, 108, 226);
-  for(let j=1;j < 6;j++){ //far right set of squares
+  for(let j=1;j < 15;j++){ //far right set of squares
 	stroke(1);
 	rect(xpos2, myMap*j, 50, 50);
   }
 
 fill(253,215,161); //Yellow squares
-  for(let j=1;j < 6;j++){ //third set of squares from the left
+  for(let j=1;j < 15;j++){ //third set of squares from the left
 	stroke(1);
 	rect(xpos3, myMap*j, 50, 50);
   }
 
   fill(235,170,220);//pink squares
-  for(let j=1;j < 6;j++){ //third set of squares from the left
+  for(let j=1;j < 15;j++){ //third set of squares from the left
 	stroke(1);
 	rect(xpos4, myMap*j, 50, 50);
   }
