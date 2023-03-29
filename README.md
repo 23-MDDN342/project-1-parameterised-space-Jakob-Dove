65dc3e0de2d29f36ecbd488c43766f72180fa643
Jakob-Dove <hackerzfuel@gmail.com>
2023-03-29 03:51:22 -0500

    Final commit for project 1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300806&assignment_repo_type=AssignmentRepo)
-### fourth Commit
+### Final Thoughts
 
-changed some colors made the background colors more complimentory towards the moving squares.  I plan for the squares to move slower.  Trying to animate the colors of the background rectangles that the squares move on. THe colors still need work and the blue needs to be changed.  I feel like something is missing as well.  
+This project is a p5.js sketch that functions as a zoom background.  The background should be able to work at any scale. The goal was to make a looping background. This project takes influence from the sunset, and candy distribution. I am pleased that I got something to loop.  I am very pleased with the colors even if the pink color is hard to see. I like that the background is bright and that all the colors kind of work together. There was a lot of experiementation to get where it needed to be. Looping the items you see on the canvas was hard to do but so rewarding when I finally figured it out! In conclusion, I am glad there is a finished project that works!
 
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,73 +1,89 @@
-//function getNoiseValue(x, y, loop, name, min, max, smoothness) {
-
-var x=300;
-var y=300;
-var a=100;
-var b=100;
 
 function draw_one_frame(cur_frac) {
-	strokeWeight(1);
- 
-  fill(255,0,0);
+	const ease = new p5.Ease();
 
-	let from = color(255,201,34);
-	let to = color(253,94, 83);
+	fill(133,180, 250);//Light blue background
+	rect(0,0,width,height);
 
-	let interA = lerpColor(from, to, 0.3);
-	let interB = lerpColor(from, to, 0.6);
-  
-  //background(73, 73, 73);
-  x+=2;
-  y+=2;
-	a-=2;
-	b-=2;
-	let myMap = map(cur_frac, 0, 2, 0, width)
-	let xpos = 30; //third set of squares from the left
-	let xpos2 = 275; //far right set of squares
-	let xpos3 = 515; //second set of squares on the left
-	let xpos4 = 755; //far left set of squares
+	let spacing = width/6 //When multiplied by x or y this should space out the shapes
+
+	let rectSpacing = width/5.65 //When multiplies by x or y this should space out the shapes
+
+	   let grid_points3 = [//Borrowed the Grid points setup code from Timothy Mitchell's pyramid code https://23-mddn342.github.io/project-1-parameterised-space-mitchetimo4/draw_one_frame.js
+	   
+		6 * width,
+		// 2 * width,
+		4 * width,
+	   -0.0425 * width
+	  
+	   ]
+
+	let startColor = color(62,120,128);//light green for the circles
+	let lastColor = color(125,240,255); //Light blue for the circles
+
+	let noiseColor;// Should swap between colors.
 	
-	let ypos = -150;
+	   fill(194,197,204)//The middle light gray rectangle
+	rect(0, height/3, width, height/3) //The large rectangle in the center of the screen.
+	   
+	   
+	 	for(let y = 0; y < 6; y++){
+			for(let i=0; i<grid_points3.length-1; i++) {///Borrowed the  for loop code from Timothy Mitchell's pyramid code https://23-mddn342.github.io/project-1-parameterised-space-mitchetimo4/draw_one_frame.js
+				let cur_x_pos = map(cur_frac, 0, 2, grid_points3[i+1], grid_points3[i])//Borrowed the for loop code from Timothy Mitchell's pyramid code https://23-mddn342.github.io/project-1-parameterised-space-mitchetimo4/draw_one_frame.js
+
+					const ease_amount_across = ease.linear(cur_frac);
+
+					
+
+					console.log((cur_x_pos+height/4.5)*.45);
+
+					noStroke();
+					
+					fill(145,148,153);//Dark gray rectangles
+					rect(0, y*spacing, width,height/6); //the skinny spaced out rectangles
+					noiseColor = getNoiseValue(cur_x_pos*5.5, (y*rectSpacing), 1, "noiseColor", .5,.7,200)
+					LerpNoise = lerpColor(startColor,lastColor, noiseColor);
+
+					fill(255,171,213);//Pink for the candy
+					ellipse((cur_x_pos+height/4.5)*.45, height/1.9, width/46, width/22); //The center of the candy wrapper.
+
+					triangle((cur_x_pos+height/4.5)*.45,height/1.95,(cur_x_pos+height/5)*.45,(height/2.15),(cur_x_pos+height/4)*.45,(height/2.15));//pink top triangle of the candy wrappper
+
+					triangle((cur_x_pos+height/4.5)*.45,height/1.85,(cur_x_pos+height/5)*.45,height/1.7,(cur_x_pos+height/4)*.45,height/1.7);//pink bottom triangle of the candy wrapper
+
+					
+					fill(255,153,102);//orange color for this piece of candy
+					
+					triangle((cur_x_pos+height/2.7)*.45,height/1.87,(cur_x_pos+height/3)*.45,(height/2.08),(cur_x_pos+height/2.4)*.45,(height/2.08));//top triangle of the square candy wrappper
+					rect((cur_x_pos+height/3)*.45, height/1.95, width/46, width/46, 5,5,5,5);//The orange candy!
+
+					//Borrowed the triangle map code from Timothy Mitchell's pyramid code https://23-mddn342.github.io/project-1-parameterised-space-mitchetimo4/draw_one_frame.js
+					
+
+					fill(20,32,255);//blue color for this piece of candy
+					
+					
+					triangle((cur_x_pos+height/12)*.45,height/1.9,(cur_x_pos+height/20)*.45,(height/2.09),(cur_x_pos+height/9)*.45,(height/2.09));//top blue triangle of the oval candy wrappper
+					triangle((cur_x_pos+height/12)*.45,height/1.9,(cur_x_pos+height/20)*.45,(height/1.75),(cur_x_pos+height/9)*.45,(height/1.75));//bottom blue triangle of the oval candy wrappper
+					ellipse((cur_x_pos+height/12)*.45, height/1.9, width/40, width/32);//The oval blue candy!
+
+					//Borrowed the triangle map code from Timothy Mitchell's pyramid code https://23-mddn342.github.io/project-1-parameterised-space-mitchetimo4/draw_one_frame.js
 
 
-	fill(interA);
-	// fill(interB);
+					push();//makes the circles visible
+					ellipseMode(CORNER);//This somewhats centers the circles in the rectangles
+					fill(LerpNoise)//makes the blue candy
+					
+					ellipse((cur_x_pos*.5), (y*rectSpacing), width/24,height/24)// makes the blue circles
+					
+					pop();
+				}
+					
 
+			
+		}
+	  
 	
-	rect(0, 0, 120, height);  //The far left rectangle
 
-	rect(240, 0, 120, height);	//The second from the left rectangle
 
-	rect(480, 0, 120, height);	//The third from the lef rectangle
-	fill(interB);
-	rect(720, 0, 120, height);		//The far right rectangle
-
-	
-	fill(255,121,79) //Orange squares
-  for(let j=1;j < 15;j++){ //third set of squares from the left
-	stroke(1);
-	rect(xpos, myMap*j, 50, 50);
-  }
-
-  fill(43, 108, 226);
-  for(let j=1;j < 15;j++){ //far right set of squares
-	stroke(1);
-	rect(xpos2, myMap*j, 50, 50);
-  }
-
-fill(253,215,161); //Yellow squares
-  for(let j=1;j < 15;j++){ //third set of squares from the left
-	stroke(1);
-	rect(xpos3, myMap*j, 50, 50);
-  }
-
-  fill(235,170,220);//pink squares
-  for(let j=1;j < 15;j++){ //third set of squares from the left
-	stroke(1);
-	rect(xpos4, myMap*j, 50, 50);
-  }
-
-ellipse(450,270, 30) // this is where zero zero is 
 }
-
-
